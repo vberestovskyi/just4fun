5ba3846fc8d1188e8a838e87de427c38850bc47f
Volodymyr Berestovskyi <vberestovskyy@gmail.com>
2019-07-09 22:47:45 +0300

    keep playing with JS

--- Temp/main.js
+++ Temp/main.js
@@ -1,22 +1,11 @@
-function Robot(name, year, owner) {
-    this.name = name;
-    this.year = year;
-    this.owner = owner;
-}
-Robot.prototype.maker = "ObjectsRUs";
-Robot.prototype.errorMessage = "All systems go.";
-Robot.prototype.reportError = function () {
-    console.log(this.name + " says " + this.errorMessage);
-};
-Robot.prototype.spillWater = function () {
-    this.errorMessage = "I appear to have a short circuit!";
-};
-var robby = new Robot("Robby", 1956, "Dr. Morbius");
-var rosie = new Robot("Rosie", 1962, "George Jetson");
-rosie.reportError();
-robby.reportError();
-robby.spillWater();
-rosie.reportError();
-robby.reportError();
-console.log(robby.hasOwnProperty("errorMessage"));
-console.log(rosie.hasOwnProperty("errorMessage"));
+let first = [1, 2, 3, 4, 5];
+let second = first;
+first.push(10);
+console.log(first); //[ 1, 2, 3, 4, 5, 10 ]
+console.log(second); //[ 1, 2, 3, 4, 5, 10 ]
+
+let third = [1, 2, 3, 4, 5];
+let forth = third;
+third = [11, 12];
+console.log(third); //[ 11, 12 ]
+console.log(forth); //[ 1, 2, 3, 4, 5 ]
